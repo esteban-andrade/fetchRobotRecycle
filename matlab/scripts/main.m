8717296db10c4c08de5373d8735f860ae83a3369
ajalsingh <ajal.singh@multifin.com.au>
2020-10-08 19:11:25 +1100

    merge

--- matlab/scripts/main.m
+++ matlab/scripts/main.m
@@ -24,20 +24,11 @@ robot.RMRC2Pose(2,0.02,pose);
 %% pick up can and raise
 robot.OpenGripper(0)
 pause(1);
-robot.RMRC2Pose(2,0.2,[pose(1), pose(2), pose(3)+0.2]);
+
+robot.RMRC2Pose(5,0.2,[pose(1), pose(2), pose(3)+0.2]);
 
 %% Go to bin
 bin_position = ros_data.getBinLocalPosition
 robot.RMRC2Pose(2,0.2,[bin_position(1), bin_position(2), bin_position(3)]);
 %% pause(3)
 robot.OpenGripper(1)
-
-%% Need to get exact bin location. This will do for now
-% drop left side
-robot.RMRC2JointState(1,0.2,[ 0.9538 -0.6826 -6.2832 0.0376 -0.0005 2.1816 -1.8031]);
-
-% drop right side
-% robot.RMRC2JointState(1,0.2,[-0.9517 -0.7973 -6.2828 0.0316 -0.0003 2.1769 -1.5690]);
-pause(3)
-robot.OpenGripper(1)
-
