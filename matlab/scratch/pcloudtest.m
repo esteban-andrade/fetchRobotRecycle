ec1cf01f1aae3ac24c681036a86c77ff039c6ad4
ajalsingh <ajal.singh@multifin.com.au>
2020-10-08 19:01:31 +1100

    ptcloud + local bin location added

--- matlab/scratch/pcloudtest.m
+++ matlab/scratch/pcloudtest.m
@@ -1,28 +1,22 @@
 %% Point cloud test
+
 addpath('../scripts/');
+hold on;
 
 ros_data = rosData;
 
-xyz = ros_data.getPointCloud;
+xyz = ros_data.getPointCloud; 
 
-pcshow(xyz)
+pc = pcshow(xyz);
+% h = scatter3(xyz(:,1),xyz(:,2),xyz(:,3))
 xlabel('X(m)')
 ylabel('Y(m)')
 zlabel('Z(m)')
 title('Detect a Cylinder in a Point Cloud')
-%% cylinder detection
-maxDistance = 1;
-roi = [0,0.5,-0.5,0.5,0.5,1];
-ptcloud =pointCloud(xyz);
-sampleIndices = findPointsInROI(ptcloud,roi);
-referenceVector = [0,0,1];
-model = pcfitcylinder(ptcloud,maxDistance,referenceVector,...
-        'SampleIndices',sampleIndices);
-hold on
-plot(model)
-%% camera calibration
-focalLength    = [554, 554]; 
-principalPoint = [320.5, 240.5];
-imageSize      = [480, 640];
-intrinsics = cameraIntrinsics(focalLength,principalPoint,imageSize);
-tagSize = 0.04;
+axis equal;
+
+%%
+while 1
+    xyz = ros_data.getPointCloud; 
+    pc = pcshow(xyz);
+end
