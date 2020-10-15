75908e893aa073383528996ce004d6ab4ef90f28
ajalsingh <ajal.singh@multifin.com.au>
2020-10-15 11:57:10 +1100

    changed spawn yaw + bug fix

--- fetch_recycle/scripts/joint_controller.py
+++ fetch_recycle/scripts/joint_controller.py
@@ -97,7 +97,7 @@ class JointController:
         trajectory.points[0].positions = arm_joint_positions
         trajectory.points[0].velocities = arm_joint_velocities
         trajectory.points[0].accelerations = [0.0 for _ in arm_joint_positions]
-        trajectory.points[0].time_from_start = rospy.Duration(0.0)
+        trajectory.points[0].time_from_start = rospy.Duration(0.0001)
 
         arm_goal = FollowJointTrajectoryGoal()
         arm_goal.trajectory = trajectory
@@ -105,7 +105,7 @@ class JointController:
 
         rospy.loginfo("Setting Arm positions...")
         self.arm_client.send_goal(arm_goal)
-        self.arm_client.wait_for_result(rospy.Duration(10.0))
+        self.arm_client.wait_for_result()
         rospy.loginfo("...done")
 
     def setGripper(self, open):
