899d593819cc2fb99ccf8e2b3190ee8d6dbf483b
Esteban <esgaanza98@gmail.com>
2020-10-22 00:33:00 +1100

    Documentation

--- matlab/scripts/fetchMotion.m
+++ matlab/scripts/fetchMotion.m
@@ -5,7 +5,8 @@ classdef fetchMotion
     end
     
     methods(Static)
-        function qMatrix = interpolateJointAnglesFetch(q1, q2, steps)
+        function qMatrix = interpolateJointAnglesFetch(q1, q2, steps) % this function will get generate a trapezoidal trajectory. 
+            %is more accurate than jtraj
             s = lspb(0, 1, steps); % First, create the scalar function
             qMatrix = nan(steps, 7); % Create memory allocation for variables
             for i = 1:steps
@@ -16,22 +17,25 @@ classdef fetchMotion
         function motion(qMatrix, robot) %pass Q matrix and corresponding robot
             
             logData = ['transforms-data.mat']; % log data into a mat file
+            %subcriber to people detection topic 
              people_sub = rossubscriber('/person_detector/status','std_msgs/Bool');
             for step = 1:size(qMatrix, 1) % iterate between rows of Q matrix
                 
-                if robot.collision.checkCollision == 0
+               
+                if robot.collision.checkCollision == 0 % check for collision
                 
-                    if robot.gui.StartButton.Value == 1
-    %                     msg = receive(people_sub);
-    %                     human_detected_status = msg.Data; 
-    %                     
-    %                     if human_detected_status==false
-    %                         robot.gui.EmergencyStopButton.Value = 1;
-    %                         robot.gui.setEstop;                 
-    %                         robot.q_before_pause = [];
-    %                         robot.active_traj = 0;
-    %                         disp('Human Detected')
-    %                     end
+                    if robot.gui.StartButton.Value == 1 %check for gui status 
+                        msg = receive(people_sub);  %get message from rostopic 
+                        human_detected_status = msg.Data; 
+                        
+                        if human_detected_status==false % if human detected false(environment is not safe)
+                            % Set emergency subroutine
+                            robot.gui.EmergencyStopButton.Value = 1;
+                            robot.gui.setEstop;                 
+                            robot.q_before_pause = [];
+                            robot.active_traj = 0;
+                            disp('Human Detected')
+                        end
 
                         %animate(robot,qMatrix(step,:));
                         robot.model.animate(qMatrix(step, :));
@@ -58,12 +62,13 @@ classdef fetchMotion
 
                         end
 
+                        
+                        drawnow() %draw robot 
+                        robot.arm_msg.Position = qMatrix(step,:); % send current joint config
+                        robot.arm_msg.Velocity=1.2;     % sent static joint vel
+                        send(robot.arm_pub,robot.arm_msg); 
 
-                        drawnow()
-                        robot.arm_msg.Position = qMatrix(step,:);
-                        robot.arm_msg.Velocity=1.2;
-                        send(robot.arm_pub,robot.arm_msg);
-
+                           %check gui for status of buttons
                         if step == size(qMatrix,1)
                             robot.q_before_pause = [];
                             robot.active_traj = 0;
@@ -93,17 +98,19 @@ classdef fetchMotion
          function RMRCmotion(qMatrix,qdot, robot) %pass Q matrix and corresponding robot
             
             logData = ['transforms-data.mat']; % log data into a mat file
+             %subcriber to people detection topic 
             people_sub = rossubscriber('/person_detector/status','std_msgs/Bool');
             for step = 1:size(qMatrix, 1) % iterate between rows of Q matrix
                 
-                if robot.collision.checkCollision == 0
+                if robot.collision.checkCollision == 0 % check for collision
                 
-                    if robot.gui.StartButton.Value == 1
+                    if robot.gui.StartButton.Value == 1 % check for gui status 
                         %animate(robot,qMatrix(step,:));
                         msg = receive(people_sub);
                         status = msg.Data;
                         
-                        if status==false
+                        if status==false % if human detected false(environment is not safe)
+                            % Set emergency subroutine
                             robot.gui.EmergencyStopButton.Value = 1;
                             robot.gui.setEstop;
                             robot.q_before_pause = [];
@@ -135,11 +142,11 @@ classdef fetchMotion
                         end
 
 
-                        drawnow()
-                        robot.arm_msg.Position = qMatrix(step,:);
-                        robot.arm_msg.Velocity=qdot(step,:)/5;
+                        drawnow()  %draw robot 
+                        robot.arm_msg.Position = qMatrix(step,:); % send joint config
+                        robot.arm_msg.Velocity=qdot(step,:)/5; % send jont vel with added gain of 0.2.
                         send(robot.arm_pub,robot.arm_msg);
-
+                           % check if current state is equal to size of q matriz for gui status 
                         if step == size(qMatrix,1)
                             robot.q_before_pause = [];
                             robot.active_traj = 0;
@@ -179,33 +186,33 @@ classdef fetchMotion
             positionError = zeros(3,robot.steps); % For plotting trajectory error
             angleError = zeros(3,robot.steps);    % For plotting trajectory error
             
-            initialPose = robot.model.fkine(robot.model.getpos);
-            initialRPY = tr2rpy(initialPose);
-            targetRPY = tr2rpy(targetPose);
+            initialPose = robot.model.fkine(robot.model.getpos); % get intial pose
+            initialRPY = tr2rpy(initialPose); % get intial RPY
+            targetRPY = tr2rpy(targetPose); % get target RPY
             
-             s = lspb(0,1,robot.steps);   
+             s = lspb(0,1,robot.steps);   % trapezoidal trajectory 
             for i=1:robot.steps
-                x(1,i) = (1-s(i))*initialPose(1, 4) + s(i)*targetPose(1, 4);
-                x(2,i) = (1-s(i))*initialPose(2, 4) + s(i)*targetPose(2, 4);
-                x(3,i) = (1-s(i))*initialPose(3, 4) + s(i)*targetPose(3, 4);
-                theta(1,i) = (1-s(i))*initialRPY(1) + s(i)*targetRPY(1);
-                theta(2,i) = (1-s(i))*initialRPY(2) + s(i)*targetRPY(2);
-                theta(3,i) = (1-s(i))*initialRPY(3) + s(i)*targetRPY(3);
+                x(1,i) = (1-s(i))*initialPose(1, 4) + s(i)*targetPose(1, 4);% Points in x
+                x(2,i) = (1-s(i))*initialPose(2, 4) + s(i)*targetPose(2, 4);% Points in y
+                x(3,i) = (1-s(i))*initialPose(3, 4) + s(i)*targetPose(3, 4);% Points in z
+                theta(1,i) = (1-s(i))*initialRPY(1) + s(i)*targetRPY(1);% Roll angle 
+                theta(2,i) = (1-s(i))*initialRPY(2) + s(i)*targetRPY(2); % Pitch angle
+                theta(3,i) = (1-s(i))*initialRPY(3) + s(i)*targetRPY(3); % Yaw angle
             end
 
-            qMatrix(1,:)= robot.model.getpos;
+            qMatrix(1,:)= robot.model.getpos; % set initial step to current pose
             
             for i = 1:robot.steps-1
                 T=robot.model.fkine(qMatrix(i,:));                                           % Get forward transformation at current joint state
                 deltaX = x(:,i+1) - T(1:3,4);                                         	% Get position error from next waypoint
                 Rd = rpy2r(theta(1,i+1),theta(2,i+1),theta(3,i+1));                     % Get next RPY angles, convert to rotation matrix
                 Ra = T(1:3,1:3);                                                        % Current end-effector rotation matrix
-                Rdot = (1/robot.deltaT)*(Rd - Ra);                                                % Calculate rotation matrix error
-                S = Rdot*Ra';  
+                Rdot = (1/robot.deltaT)*(Rd - Ra);                                       % Calculate rotation matrix error
+                S = Rdot*Ra';                                                           % Skew symmetric
                 linear_velocity = (1/robot.deltaT)*deltaX;
                 angular_velocity = [S(3,2);S(1,3);S(2,1)];                              % Check the structure of Skew Symmetric matrix!!
                 deltaTheta = tr2rpy(Rd*Ra');                                            % Convert rotation matrix to RPY angles
-                xdot = W*[linear_velocity;angular_velocity];
+                xdot = W*[linear_velocity;angular_velocity];                            % Calculate end-effector velocity to reach next waypoint
                 J = robot.model.jacob0(qMatrix(i,:));                                          % Get Jacobian at current joint state
                 m(i) = sqrt(det(J*J'));
                 if m(i) < robot.epsilon  % If manipulability is less than given threshold
@@ -215,9 +222,10 @@ classdef fetchMotion
                 end
                 
                 N = null(J);
-                invJ = pinv(J'*J + lambda *eye(7))*J';
-                qdot(i,:) = (invJ*xdot)';
-                for j = 1:7
+                invJ = pinv(J'*J + lambda *eye(7))*J';  % DLS Inverse
+                qdot(i,:) = (invJ*xdot)';  % Solve the RMRC equation (you may need to transpose the vector)
+                for j = 1:7 % Loop through joints 1 to 7
+                    % check status for joints 1,3,5 and add extra DLS for extra safety of motion.  
                     if j == 1 || j == 3 || j == 5
                         if qMatrix(i,j) >= 180
                             qMatrix(i,j) = mod(qMatrix(i,j), 180);
@@ -225,32 +233,34 @@ classdef fetchMotion
                             qMatrix(i,j) = mod(qMatrix(i,j), 180);
                         end
                     end
-                    if qMatrix(i,j) + robot.deltaT*qdot(i,j) < robot.model.qlim(j,1)
-                        qdot(i,j) = 0;
-                    elseif qMatrix(i,j) + robot.deltaT*qdot(i,j) > robot.model.qlim(j,2)
-                        qdot(i,j) = 0;
+                    if qMatrix(i,j) + robot.deltaT*qdot(i,j) < robot.model.qlim(j,1) % If next joint angle is lower than joint lim
+                        qdot(i,j) = 0; % stop motor
+                    elseif qMatrix(i,j) + robot.deltaT*qdot(i,j) > robot.model.qlim(j,2)  % If next joint angle is greater than joint limit
+                        qdot(i,j) = 0; %stop motor
                     end
                 end
                 qMatrix(i+1,:) = qMatrix(i,:) + robot.deltaT*qdot(i,:);                         	% Update next joint state based on joint velocities
-                positionError(:,i) = x(:,i+1) - T(1:3,4);                               % For plotting
+                positionError(:,i) = x(:,i+1) - T(1:3,4);                              
                 angleError(:,i) = deltaTheta;                
                 
             end                                           
             
         end
-        %%
+        %% function to calculate the time based on pose 
         function time= calculateTime(T, canpose)
+            % get distance between transform and target pose 
             dist = norm(T(1:3,4)' - canpose(:));
             
+            % constant and constrans 
             max_steps = 50;
             min_steps = 5;
             max_dist = 0.94;
             min_dist = 0.01;
             deltaT = 0.02;
-            
-            gain = (dist - min_dist)/max_dist;
-            steps = gain*max_steps + min_steps;
-            time = steps * deltaT;
+            %eqation based on slope. Linear approach for result y =mx+b
+            gain = (dist - min_dist)/max_dist; % get m 
+            steps = gain*max_steps + min_steps; % resolve for number of steps 
+            time = steps * deltaT; % get time to be used on RMRC
         end
         
     end
